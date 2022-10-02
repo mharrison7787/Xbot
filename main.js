ffc294eecc55a1138edd687563166ad007241147
Trevor Foresta <trevforesta@gmail.com>
2022-10-02 18:44:26 -0400

    Working version
    Prof. X bot now sends a random quote when the specified prefixes are said.
    
    Co-Authored-By: mharrison7787 <112901853+mharrison7787@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,21 +1,37 @@
 var Discord = require('discord.js');
-var client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
-
-client.once('ready', () => {
-    console.log('Logged in!');
+const client = new Discord.Client({
+	intents: ['GUILDS', 'GUILD_MESSAGES'],
 });
 
-//Message - current usage: !msg pings the bot to say "Hello World!"
-//Change as needed
-client.on('message', message => {
-    var prefix = '!'
-    var msg = message.content;
-    if (msg === prefix + 'msg') {
-        message.channel.send(`Hello World!`)
-        .then(console.log)
-        .catch(console.error);
-    }
+console.log('Logged in!');
+//client.once('ready', () => {
+//    console.log('Logged in!');
+//});
+const fs = require('fs');
+
+//function to get random quote from quotes.txt
+function getRandomQuote() {
+	var data = fs.readFileSync('quotes.txt', 'utf8'); // reads the file  as a buffer
+	data = data.toString(); // makes the buffer into a readable string
+	data = data.split('\n'); // splits the string into an array of strings
+	var randomQuote = data[Math.floor(Math.random() * data.length)]; // gets a random quote from the array
+	return randomQuote; // returns the random quote
+}
+
+client.on('messageCreate', (message) => {
+	const msgQuote = String(getRandomQuote());
+	console.log(message.content);
+	console.log(msgQuote);
+	if (
+		message.content === '!x' ||
+		message.content === 'x' ||
+		message.content === 'Dr. X'
+	) {
+		message.channel.send(msgQuote);
+	}
 });
 
 //Note: change 'token' to bot's token
-client.login('OTg0MjI1NzgyOTYyNDU4Njg0.GE7ICi.00dk335LkNeeJ4nvyKgc_y50tnO7tU0yzkgY0c');
+client.login(
+	'OTg0MjI1NzgyOTYyNDU4Njg0.GE7ICi.00dk335LkNeeJ4nvyKgc_y50tnO7tU0yzkgY0c'
+);
